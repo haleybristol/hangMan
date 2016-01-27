eea0227dae72d015dca9b7778dd1769c01de7a82
Afton Downey and Haley Bristol <aaike001@gmail.com>
2016-01-27 11:38:25 -0800

    add specs for splitting words and finding letter index

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
-function Random (fruit){
+// function hangMan (words, letters, fails, discards)
+
+
+
+function Random (fruit) {
   var fruit = ["apple"] //"banana", "berry", "orange"];
   var word = fruit[Math.floor(Math.random() * fruit.length)];
   return word;
@@ -12,4 +16,24 @@ function Replace (word) {
 function splitWord (word) {
   var splitWord = word.split("");
   return splitWord;
+};
+
+function splitBlank (newWord) {
+  var splitBlank = newWord.split("");
+  return splitBlank;
+};
+
+function Index (splitWord) {
+  var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"];
+  var index = splitWord.indexOf(letters);
+  return index;
+};
+
+function Discard (letters, index) {
+  var discards = [];
+  if (index === -1) {
+    discards.push(letters);
+  } else {
+    return index;
+  }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,3 +1,13 @@
+// describe ("hangMan", function() {
+//   it("will create a new game with applicable information", function() {
+//   var testhangMan = new hangMan();
+//   expect(testhangMan.words).to.eql([]);
+//   expect(testhangMan.letters).to.eql([]);
+//   expect(testhangMan.fails).to.eql([]);
+//   expect(testhangMan.discards).to.eql([]);
+//   });
+// });
+
 describe("Random", function() {
 
   it("will select a random word from an array", function() {
@@ -10,5 +20,16 @@ describe("Random", function() {
 
   it("will split word into an array of letters", function() {
     expect(splitWord("apple")).to.eql(["a","p","p","l","e"]);
+  });
+
+  it("it will split blanks into an array of blanks", function() {
+    expect(splitBlank("_____")).to.eql([ '_', '_', '_', '_', '_' ]);
+  });
+
+  it("will return the index position of a given letter", function() {
+    expect(Index("a")).to.equal(0);
+  });
+  it("will push a letter that is not found into a new array", function() {
+    expect(Discard("b", -1)).to.eql(["b"]);
   })
 });
